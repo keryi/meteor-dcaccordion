bf41d1af314364b984b9510beb03c5aa9bd2cbd8
keryi <hti_32@yahoo.com>
2015-06-10 21:53:52 +0800

    Changed to include un-minified file instead

--- package.js
+++ package.js
@@ -11,5 +11,5 @@ Package.onUse(function(api) {
   api.use('jquery');
   api.addFiles('lib/jquery-cookie/src/jquery.cookie.js', 'client');
   api.addFiles('lib/jquery-hoverIntent/jquery.hoverIntent.js', 'client');
-  api.addFiles('lib/jquery.dcaccordion/jquery.dcaccordion.2.7.min.js', 'client');
+  api.addFiles('lib/jquery.dcaccordion/jquery.dcaccordion.2.7.js', 'client');
 });
